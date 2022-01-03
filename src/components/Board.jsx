053664a658b67d710143e92eea46c436631084d3
Figarillo <axel.leonardi@hotmail.com>
2022-01-03 13:00:27 -0300

    Desarrollo funcionalidad volver a una jugada anterior

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,41 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-export default function Board() {
-	const [state, setState] = useState({ squares: Array(9).fill(null), xIsNext: true });
-
-	const handleClick = i => {
-		const squares = state.squares.slice();
-		if (calculateWinner(squares) || squares[i]) return;
-		squares[i] = state.xIsNext ? 'X' : 'O';
-		setState({ squares: squares, xIsNext: !state.xIsNext });
-	};
-
-	const calculateWinner = squares => {
-		const lines = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		for (let i = 0; i < lines.length; i++) {
-			const [a, b, c] = lines[i];
-			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-				return squares[a];
-			}
-		}
-		return null;
-	};
-
-	let status = `Next player:  ${state.xIsNext ? 'X' : 'O'}`;
-	if (calculateWinner(state.squares)) status = `Winner ${calculateWinner(state.squares)}`;
-
+export default function Board(props) {
 	const renderSquare = i => (
-		<Square value={state.squares[i]} onClick={() => handleClick(i)} />
+		<Square value={props.squares[i]} onClick={() => props.onClick(i)} />
 	);
 
 	const boardStyle = {
@@ -58,7 +26,6 @@ export default function Board() {
 	return (
 		<div>
 			<div style={flexContainer}>
-				<div className="status">{status}</div>
 				<div style={boardStyle}>
 					{renderSquare(0)}
 					{renderSquare(1)}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,28 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 
 export default function Game() {
+	/* CSS STYLES */
+
 	const mainStyle = {
-		textAlign: 'center',
-		backgroundColor: '#282c34',
 		minHeight: '100vh',
 		display: 'flex',
+		justifyContent: 'flex-start',
 		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'space-beetwen',
+		textAlign: 'center',
 		fontSize: 'calc(10px + 2vmin)',
-		color: 'white',
+		backgroundColor: '#282c34',
+		color: '#fff',
 	};
+
+	const gameStyle = {
+		minHeight: '100%',
+		display: 'flex',
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+	};
+
+	const btnStyle = {
+		backgroundColor: 'transparent',
+		border: '1px solid #ddd',
+		color: '#fff',
+		padding: '0.6em',
+		borderRadius: '5px',
+	};
+
+	const [state, setState] = useState({
+		history: [{ square: Array(9).fill(null) }],
+		stepNumber: 0,
+		xIsNext: true,
+	});
+
+	// Se realiza la destructuracion para obtener el objeto history el cual es un array
+	const history = state.history;
+	// A traves del objeto history, se extra la última posición del arregle, la cual siempre va a corresponder con la posisción actual
+	const current = history[state.stepNumber]; // Ej -> current = square: [X, O, null, ... ,null]
+
+	const calculateWinner = squares => {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+			}
+		}
+		return null;
+	};
+
+	const handleClick = i => {
+		const history = state.history.slice(0, state.stepNumber + 1);
+		const current = history[history.length - 1];
+		// Se crea una copia del arreglo square de la actual posicion
+		const squares = current.square.slice(); // Ej -> square = [X, O, null, ... ,null]
+		if (calculateWinner(squares) || squares[i]) return; // Cortar funcionamiento si existe un ganador
+		// Se asigna X o O, según xIsNext, a la posicion de squares pasada por parametro
+		squares[i] = state.xIsNext ? 'X' : 'O';
+		setState({
+			history: history.concat([{ square: squares }]), // A history se le añade el contenido de squares
+			stepNumber: history.length,
+			xIsNext: !state.xIsNext,
+		});
+	};
+
+	const jumpTo = step => {
+		setState({
+			history: state.history,
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
+		});
+	};
+
+	// Decir ganador
+	const winner = calculateWinner(current.square);
+	let status = `Next player:  ${state.xIsNext ? 'X' : 'O'}`;
+	if (winner) status = `Winner ${winner}`;
+
+	const moves = history.map((step, move) => {
+		const desc = move ? `Go to  move #${move}` : `Go to game start`;
+		return (
+			<li key={move}>
+				<button style={btnStyle} onClick={() => jumpTo(move)}>
+					{desc}
+				</button>
+			</li>
+		);
+	});
 
 	return (
 		<div style={mainStyle}>
 			<h1>TIC TAC TOE</h1>
-			<div>
-				<Board />
-			</div>
-			<div>
-				<div>{/* status */}</div>
-				<ol>{/* TODO */}</ol>
+			<div style={gameStyle}>
+				<div>
+					<div>{status}</div>
+				</div>
+				<div>
+					<Board squares={current.square} onClick={i => handleClick(i)} />
+				</div>
+				<div>
+					<ol style={{ listStyle: 'none' }}>{moves}</ol>
+				</div>
 			</div>
 		</div>
 	);
